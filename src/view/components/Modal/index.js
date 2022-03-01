db43c2ac60c757973cc7768538c598f2badc9c14
David Petric <david.petric99@gmail.com>
2022-03-01 20:13:02 +0200

    Modal fix

--- src/view/components/Modal/index.js
+++ src/view/components/Modal/index.js
@@ -1,16 +1,14 @@
 const Modal = (props) => {
   return props.visible === true ? (
-    <div
-      onClick={() => props.setVisible(false)}
-      className="fixed z-10 inset-0 overflow-y-auto"
-    >
+    <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          onClick={() => props.setVisible(false)}
+        ></div>
         <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
           &#8203;
         </span>
-
         <div className="inline-block align-bottom bg-white text-black rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div className="flex justify-center px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="flex flex-col mt-3 text-center justify-center">
