e197a361ba7793d91708e3bc52e2c8b3aa0282a6
David Petric <david.petric99@gmail.com>
2022-04-15 18:05:19 +0300

    FETCH USERS

--- src/view/Application.js
+++ src/view/Application.js
@@ -1,9 +1,24 @@
 import { Footer, Navbar } from "./components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import routes from "../logic/routes";
 
 const Application = () => {
+  const [users, setUsers] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [users2, setUsers2] = useState([]);
+  const [selectedUser, setSelecterdUser] = useState({});
+
+  const API = "http://localhost:3000/api/v1/users";
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => setUsers(res));
+  }, []);
+  console.log(users);
+
   return (
     <BrowserRouter>
       <div className="flex flex-col justify-between min-h-screen">
